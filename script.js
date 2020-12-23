b7afd9512cb6d96b551dd9fbe9611d222efc4af0
uzunyusuf <yusufuzn19@gmail.com>
2020-12-23 20:05:08 +0300

    finally project

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
   });
 
   // toggle menu/navbar script
